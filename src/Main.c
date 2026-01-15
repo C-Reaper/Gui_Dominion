03d21d1459a021a2b88495231be255b57edaf3ef
codeleaded <gnu.c.reaper@gmail.com>
2026-01-15 17:40:35 +0100

    init commit

---
+++ src/Main.c
@@ -0,0 +1,22 @@
+//#include "C:/Wichtig/System/Static/Library/WindowEngine1.0.h"
+#include "/home/codeleaded/System/Static/Library/WindowEngine1.0.h"
+#include "/home/codeleaded/System/Static/Library/Random.h"
+
+
+void Setup(AlxWindow* w){
+	
+}
+
+void Update(AlxWindow* w){
+	Clear(BLACK);
+}
+
+void Delete(AlxWindow* w){
+	
+}
+
+int main(){
+    if(Create("Dominion",2500,1200,1,1,Setup,Update,Delete))
+        Start();
+    return 0;
+}
